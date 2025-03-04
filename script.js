719b28a4ffcb31aacf2d725b5deef58049943daa
Hannrhiee <hmlcaduada22@src.pshs.edu.ph>
2025-03-04 21:36:06 +0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function verMessage() {
+    let messageToVer = prompt("Enter a word to verify:");
+
+    if (!messageToVer) {
+        document.getElementById('result').innerHTML = "No message entered.";
+        return;
+    }
+
+    if (messageToVer.includes("FR") && messageToVer.includes("AI")) {
+        document.getElementById('result').innerHTML = `The message "${messageToVer}" is legitimate!`;
+    }  
+    else if (messageToVer.includes("AI") || messageToVer.includes("aI") || messageToVer.includes("Ai")) {
+        document.getElementById('result').innerHTML = `The message "${messageToVer}" is tampered!!`;
+    }  
+    else if (messageToVer.includes("FR")) {
+        document.getElementById('result').innerHTML = `The message "${messageToVer}" is legitimate!`;
+    }  
+    else {
+        document.getElementById('result').innerHTML = `The message "${messageToVer}" is not yet encoded!!`;
+    }
+}
